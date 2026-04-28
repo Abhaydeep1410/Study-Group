be084dc701c5ef0067f627d09092f7dd6c239107
Abhay Deep <abhay9456234282@gmail.com>
2026-04-28 23:13:05 +0530

    abstract factory

--- Study-Group/src/patterns/Day6_AbstractFactory.java
+++ Study-Group/src/patterns/Day6_AbstractFactory.java
@@ -1,4 +1,24 @@
 package patterns;
+interface UIFactory {
+    Button createButton();
+    Checkbox createCheckbox();
+}
+class WindowsFactory implements UIFactory {
+    public Button createButton() {
+        return new WindowsButton();
+    }
+    public Checkbox createCheckbox() {
+        return new WindowsCheckbox();
+    }
+}
 
+class MacFactory implements UIFactory {
+    public Button createButton() {
+        return new MacButton();
+    }
+    public Checkbox createCheckbox() {
+        return new MacCheckbox();
+    }
+}
 public class Day6_AbstractFactory {
 }
